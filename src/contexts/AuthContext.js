3eed0709da50b3fa05ad5910c3d80a155d79bd93
jpeart03 <james.r.peart@gmail.com>
2021-09-20 22:15:49 -0500

    add vv0.1 auth context methods

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,17 +1,103 @@
 import React, { useContext, useState, useEffect } from "react";
+import axios from "axios";
 
+const storageKey = "userToken";
+const apiUrl = process.env.REACT_APP_API_URL;
 const AuthContext = React.createContext();
 export const useAuth = () => useContext(AuthContext);
 
 export const AuthProvider = ({ children }) => {
-  const [authLoading, setAuthLoading] = useState();
+  const [authLoading, setAuthLoading] = useState(false);
+  const [authToken, setAuthToken] = useState();
   const [currentUser, setCurrentUser] = useState();
 
-  const register = () => "register called";
+  const checkStorageToken = () => {
+    console.log("checkStorageToken fired!");
+    const tokenString = localStorage.getItem(storageKey)
+      ? `Token ${localStorage.getItem(storageKey)}`
+      : null;
+    setAuthToken(tokenString);
+  };
 
-  const login = () => "login called";
+  // Adding useEffect to grab token from localstorage & set AuthProvider state
+  useEffect(() => {
+    checkStorageToken();
+    const getUserData = async () => {
+      if (authToken) {
+        const { data } = await axios.get(`${apiUrl}/users/auth/user/`, {
+          headers: {
+            Authorization: authToken,
+          },
+        });
+        // TODO check for successful response... handle errors.
+        setCurrentUser(data);
+      }
+    };
+    getUserData();
+  }, [authToken]);
 
-  const logout = () => "logout called";
+  /**
+   * Function to register a new user
+   * @param {string} username
+   * @param {string} email
+   * @param {string} password1
+   * @param {string} password2
+   */
+  const register = async (username, email, password1, password2) => {
+    axios
+      .post(`${apiUrl}/users/auth/register/`, {
+        username: username,
+        email: email,
+        password1: password1,
+        password2: password2,
+      })
+      .then((response) => {
+        localStorage.setItem(storageKey, response.data.key);
+        setAuthToken(`Token ${response.data.key}`);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  /**
+   * Function to log in user
+   * @param {string} email user's email
+   * @param {string} password user's password
+   */
+  const login = async (email, password) => {
+    await axios
+      .post(`${apiUrl}/users/auth/login/`, {
+        email: email,
+        password: password,
+      })
+      .then((response) => {
+        localStorage.setItem(storageKey, response.data.key);
+        setAuthToken(`Token ${response.data.key}`);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  /**
+   * Function to log user out
+   */
+  const logout = async () => {
+    if (authToken) {
+      await axios
+        .post(`${apiUrl}/users/auth/logout/`, {
+          headers: {
+            Authorization: authToken,
+          },
+        })
+        .then(() => {
+          localStorage.removeItem(storageKey);
+          setAuthToken();
+          setCurrentUser();
+        });
+    }
+  };
 
   const value = {
     authLoading,
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,29 @@
 import { Button, Row, Col } from "react-bootstrap";
+import { useAuth } from "../contexts/AuthContext";
 import "./Home.scss";
 
 const Home = () => {
+  const { register, login, logout, currentUser } = useAuth();
+
   return (
     <>
       <section className="section section-hero">
         <div>
           <h1>Appreciation App</h1>
+          <p>{currentUser && currentUser.email}</p>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus eu
             varius est. Nunc libero urna, tincidunt eget enim quis, sollicitudin
             imperdiet eros. Morbi malesuada non ante ut eleifend. Suspendisse
             ultrices faucibus magna eget dictum. Aliquam a mi tellus.
           </p>
-          <Button variant="primary">Sign Up!</Button>
+          <Button
+            variant="primary"
+            // onClick={() => login("Test1@test.com", "Test123!")}
+            onClick={() => logout()}
+          >
+            Sign Up!
+          </Button>
         </div>
       </section>
       <section className="section section-demo">
