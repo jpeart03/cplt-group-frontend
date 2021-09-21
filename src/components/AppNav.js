f595582463460db65460e3e1134725df0505bfc3
jpeart03 <james.r.peart@gmail.com>
2021-09-21 15:11:41 -0500

    add auth context to AppNav

--- src/components/AppNav.js
+++ src/components/AppNav.js
@@ -1,15 +1,33 @@
 import { Container, Navbar } from "react-bootstrap";
+import LoadingButton from "../components/LoadingButton/LoadingButton";
+import { useAuth } from "../contexts/AuthContext";
 const AppNav = () => {
+  const { currentUser, authLoading, login, logout } = useAuth();
+
   return (
     <Navbar bg="light" variant="light" expand="lg" sticky="top">
       <Container>
         <Navbar.Brand>Appreciation</Navbar.Brand>
         <Navbar.Toggle />
         <Navbar.Collapse className="justify-content-end">
-          <Navbar.Text>
-            {/* TODO: Conditional login/logged in as text here */}
-            Welcome, Bob
-          </Navbar.Text>
+          {currentUser ? (
+            <>
+              <Navbar.Text>Welcome, {currentUser.email}</Navbar.Text>
+              <LoadingButton
+                text="Log out"
+                variant="secondary"
+                isLoading={authLoading}
+                onClick={() => logout()}
+              />
+            </>
+          ) : (
+            <LoadingButton
+              text="Log in"
+              variant="secondary"
+              isLoading={authLoading}
+              onClick={() => login("Test1@test.com", "Test123!")}
+            />
+          )}
         </Navbar.Collapse>
       </Container>
     </Navbar>
