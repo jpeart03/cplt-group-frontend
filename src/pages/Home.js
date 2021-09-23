17864986c3f6028da8a381bf89d4510709be1ec3
jpeart03 <james.r.peart@gmail.com>
2021-09-23 12:45:57 -0500

    add homepage section content

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,8 @@ import { useAuth } from "../contexts/AuthContext";
 import LoadingButton from "../components/LoadingButton/LoadingButton";
 import "./Home.scss";
 import HomeChart from "../components/HomeChart/HomeChart";
-import { Link } from "react-router-dom";
+import mobileDemoImg from "../images/mobile-demo.svg";
+import webDemoImg from "../images/web-demo.svg";
 
 const Home = () => {
   const { register, login, logout, currentUser, authLoading } = useAuth();
@@ -31,20 +32,39 @@ const Home = () => {
         </div>
       </section>
       <section className="section section-demo">
-        <p>TODO: MESSAGE DEMO VIDEO HERE</p>
+        <Row>
+          <Col lg={4}>
+            <h2>How the app works</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+              eu varius est. Nunc libero urna, tincidunt eget enim quis,
+              sollicitudin imperdiet eros. Morbi malesuada non ante ut eleifend.
+            </p>
+          </Col>
+          <Col lg={8} className="graphic">
+            <picture>
+              <source media="(min-width: 992px)" srcSet={webDemoImg} />
+              <source media="(max-width: 993px)" srcSet={mobileDemoImg} />
+              <img
+                src={mobileDemoImg}
+                alt="Graphic demonstrating the message function of the application."
+              />
+            </picture>
+          </Col>
+        </Row>
       </section>
       <section className="section section-data">
         <Row>
-          <Col lg={6} className="graphic">
-            <HomeChart />
-          </Col>
-          <Col lg={6} className="text">
+          <Col lg={4} className="text">
             <h2>About the data</h2>
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
               eu varius est. Nunc libero urna, tincidunt eget enim quis,
               sollicitudin imperdiet eros. Morbi malesuada non ante ut eleifend.
             </p>
+          </Col>
+          <Col lg={8} className="chart">
+            <HomeChart />
           </Col>
         </Row>
       </section>
