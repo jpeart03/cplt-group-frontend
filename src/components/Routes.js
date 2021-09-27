c1b033180e47589116ef94247432ec0c64698620
jpeart03 <james.r.peart@gmail.com>
2021-09-27 14:47:08 -0500

    add ProtectedRoute component & update Routes component

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,26 +1,30 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "../pages/Home";
-import RegisterLoginPage from "../pages/RegisterLoginPage"
-import UserDashboardPage from "../pages/UserDashboardPage"
-import NewMessagePage from "../pages/NewMessagePage.js"
+import RegisterLoginPage from "../pages/RegisterLoginPage";
+import UserDashboardPage from "../pages/UserDashboardPage";
+import NewMessagePage from "../pages/NewMessagePage.js";
 import RecipientsPage from "../pages/RecipientsPage";
 import Layout from "./Layout";
-
+import NotFoundPage from "../pages/NotFoundPage";
+import ProtectedRoute from "./ProtectedRoute/ProtectedRoute";
 
 const Routes = () => {
   return (
     <BrowserRouter>
-    <Layout>
-      {/* TODO: Add AppNav component */}
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/login" component={RegisterLoginPage} />
-        <Route exact path="/register" component={RegisterLoginPage} />
-        <Route exact path="/dashboard" component={UserDashboardPage}/>
-        <Route exact path="/newmessage" component={NewMessagePage}/>
-        <Route exact path="/recipients" component={RecipientsPage}/>
-        {/* Add More Routes Here */}
-      </Switch>
+      <Layout>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/login" component={RegisterLoginPage} />
+          <Route exact path="/register" component={RegisterLoginPage} />
+          <ProtectedRoute
+            exact
+            path="/dashboard"
+            component={UserDashboardPage}
+          />
+          <ProtectedRoute exact path="/newmessage" component={NewMessagePage} />
+          <ProtectedRoute exact path="/recipients" component={RecipientsPage} />
+          <Route component={NotFoundPage} />
+        </Switch>
       </Layout>
     </BrowserRouter>
   );
---
+++ src/pages/NotFoundPage.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const NotFoundPage = () => {
+  // TODO: Add real content
+  return <div>Page not found...</div>;
+};
+
+export default NotFoundPage;
