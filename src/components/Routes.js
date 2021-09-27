fe5a3bf2a13c5fd223347e5b64d97b972823151e
jpeart03 <james.r.peart@gmail.com>
2021-09-27 10:39:17 -0500

    fix message form

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,26 +1,27 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "../pages/Home";
-import RegisterLoginPage from "../pages/RegisterLoginPage"
-import UserDashboardPage from "../pages/UserDashboardPage"
-import NewMessagePage from "../pages/NewMessagePage.js"
+import RegisterLoginPage from "../pages/RegisterLoginPage";
+import UserDashboardPage from "../pages/UserDashboardPage";
+import NewMessagePage from "../pages/NewMessagePage.js";
 import RecipientsPage from "../pages/RecipientsPage";
 import Layout from "./Layout";
-
+import NewMessage from "../pages/NewMessage";
 
 const Routes = () => {
   return (
     <BrowserRouter>
-    <Layout>
-      {/* TODO: Add AppNav component */}
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/login" component={RegisterLoginPage} />
-        <Route exact path="/register" component={RegisterLoginPage} />
-        <Route exact path="/dashboard" component={UserDashboardPage}/>
-        <Route exact path="/newmessage" component={NewMessagePage}/>
-        <Route exact path="/recipients" component={RecipientsPage}/>
-        {/* Add More Routes Here */}
-      </Switch>
+      <Layout>
+        {/* TODO: Add AppNav component */}
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/login" component={RegisterLoginPage} />
+          <Route exact path="/register" component={RegisterLoginPage} />
+          <Route exact path="/dashboard" component={UserDashboardPage} />
+          {/* <Route exact path="/newmessage" component={NewMessagePage}/> */}
+          <Route exact path="/newmessage" component={NewMessage} />
+          <Route exact path="/recipients" component={RecipientsPage} />
+          {/* Add More Routes Here */}
+        </Switch>
       </Layout>
     </BrowserRouter>
   );
---
+++ src/pages/NewMessage.js
@@ -0,0 +1,93 @@
+import React, { useState, useEffect } from "react";
+import { Form } from "react-bootstrap";
+import { useLocation } from "react-router-dom";
+import LoadingButton from "../components/LoadingButton/LoadingButton";
+import { useAuth } from "../contexts/AuthContext";
+import "./NewMessage.scss";
+
+const NewMessage = () => {
+  const { authToken, currentUser } = useAuth();
+  const location = useLocation();
+  const historyStateRecipient = location.state?.recipient;
+
+  const [recipients, setRecipients] = useState([
+    { id: 1, first_name: "Jim", last_name: "Peart" },
+  ]);
+  const [selectedRecipient, setSelectedRecipient] = useState();
+  const [messageContent, setMessageContent] = useState();
+  const [sendEmail, setSendEmail] = useState(false);
+  const [sendSMS, setSendSMS] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (historyStateRecipient) {
+      setSelectedRecipient(historyStateRecipient.id);
+    }
+    // TODO: get recipients from API and setRecipients
+  }, [historyStateRecipient]);
+
+  const handleSendMessage = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    // TODO: call api to post message
+    console.log({ selectedRecipient, messageContent, sendEmail, sendSMS });
+    setLoading(false);
+  };
+
+  return (
+    <div className="message">
+      <h1>Send an Appreciation Message</h1>
+      <Form onSubmit={(e) => handleSendMessage(e)}>
+        {/* Recipient Select */}
+        <Form.Group className="mb-3">
+          <Form.Label>Recipient</Form.Label>
+          <Form.Select
+            aria-label="Recipient select"
+            value={selectedRecipient}
+            onChange={(e) => setSelectedRecipient(e.currentTarget.value)}
+          >
+            <option>Select a message recipient</option>
+            {recipients.map((recipient) => (
+              <option key={recipient.id} value={recipient.id}>
+                {recipient.first_name} {recipient.last_name}
+              </option>
+            ))}
+          </Form.Select>
+        </Form.Group>
+        {/* Delivery Method */}
+        <Form.Group className="mb-3">
+          <Form.Check
+            type="checkbox"
+            label="Email"
+            checked={sendEmail}
+            onChange={(e) => setSendEmail(e.currentTarget.checked)}
+          />
+          <Form.Check
+            type="checkbox"
+            label="Text Message"
+            checked={sendSMS}
+            onChange={(e) => setSendSMS(e.currentTarget.checked)}
+          />
+        </Form.Group>
+        {/* Message */}
+        <Form.Group className="mb-3">
+          <Form.Label>Write your Appreciation Message</Form.Label>
+          <Form.Control
+            as="textarea"
+            rows={3}
+            value={messageContent}
+            onChange={(e) => setMessageContent(e.currentTarget.value)}
+          />
+        </Form.Group>
+        <LoadingButton
+          variant="primary"
+          type="submit"
+          loading={loading}
+          text="Send"
+        />
+      </Form>
+    </div>
+  );
+};
+
+export default NewMessage;
