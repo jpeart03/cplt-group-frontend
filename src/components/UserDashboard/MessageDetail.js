619c966a73967012e52e72af8b11983e17e148ad
JessMelvilleGoodin <8205019+JessMelvilleGoodin@users.noreply.github.com>
2021-09-30 10:51:49 -0400

    added contact number/email address to messageDetail card

--- src/components/UserDashboard/MessageDetail.js
+++ src/components/UserDashboard/MessageDetail.js
@@ -16,7 +16,6 @@ const MessageDetail = ({messageObj}) => {
     let x = await fetchRecipientByID(recipientID, authToken)
     setRecipientName(x.first_name)
     setRecLastName(x.last_name)
-    setdeliveryContactInfo("(dummy info) 309-654-5555")
   }
 
   useEffect( () => {
@@ -28,13 +27,30 @@ const MessageDetail = ({messageObj}) => {
   let dateOnly = messageObj.send_date.split(' ')[0]
   let messageBody = messageObj.content
 
+  const ContactInfo = () => {
+    if (messageObj.send_email && messageObj.send_sms){
+      return (
+      <>
+        <h6>At: {messageObj.sent_to_email} and {messageObj.sent_to_email}</h6>
+      </>
+      )
+    }
+    else if (messageObj.send_email){
+      return <h6>At: {messageObj.sent_to_email}</h6>
+    }
+    else if (messageObj.send_sms){
+      return <h6>At: {messageObj.sent_to_phone}</h6>
+    }
+    else return null
+  }
+
   return (
     <div>
       <Card style={{display:"flex", flexDirection:"row"}}>
         <div style={{width:"18%", marginLeft:"10%"}}><h6>{dateOnly}</h6></div>
         <div style={{width:"45%"}}>
           <h6>To: {recipientName} {recLastName}</h6>
-          <h6>At: {deliveryContactInfo}</h6>
+          <ContactInfo />
           <p>{messageBody}</p>
         </div>
   
