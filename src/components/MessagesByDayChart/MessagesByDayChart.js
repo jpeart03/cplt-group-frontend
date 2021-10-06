19309baa64bcb20671e309e3f1d75c3436376fb6
adamziolk <adam.lawrence.ziolkowski@gmail.com>
2021-10-05 20:18:10 -0600

    basically working

--- src/components/MessagesByDayChart/MessagesByDayChart.js
+++ src/components/MessagesByDayChart/MessagesByDayChart.js
@@ -19,24 +19,37 @@ const MessagesByDayChart = () => {
 
   }, [authToken])
 
-  // const data = [{name: 'Page A', uv: 400, pv: 2400, amt: 2400}];
-  // const data = messagesByDayData.map( (idx) => console.log("MAP", idx))
-  // console.log("MBDD", messagesByDayData['20210928'])
 
   const renderLineChart = (
-    <LineChart width={600} height={300} data={messagesByDayData}>
-      <Line type="monotone" dataKey="uv" stroke="#8884d8" />
-      <CartesianGrid stroke="#ccc" />
+    <LineChart width={600} height={300} data={messagesByDayData} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+      <Line type="monotone" dataKey="value" stroke="#8884d8" />
+      <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
       <XAxis dataKey="name" />
       <YAxis />
     </LineChart>
   );
 
+  if(messagesByDayData) {
+    // console.log("start MBDD", messagesByDayData)
 
+    messagesByDayData.sort(function(a, b) {
+      var keyA = a.name,
+        keyB = b.name;
+        // console.log("keyA", keyA, "keyB", keyB)
+      // Compare the 2 dates
+      if (keyA < keyB) return -1;
+      if (keyA > keyB) return 1;
+      return 0;
+    });
+
+    // console.log("end MBDD", messagesByDayData)
+
+  }
 
   return (
+
     <div>
-      <h1>THIS IS WHERE THE CHART GOES</h1>
+      <h2>Recent Message Activity</h2>
       {renderLineChart}
     </div>
   )
