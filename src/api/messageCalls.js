3b692b8c4504eb4b9f06ac2f42157897ece04296
JessMelvilleGoodin <8205019+JessMelvilleGoodin@users.noreply.github.com>
2021-10-09 16:02:30 -0400

    Recipient Chart will update when a message is sent without refresh

--- src/api/messageCalls.js
+++ src/api/messageCalls.js
@@ -1,3 +1,5 @@
+import { fetchRecipientsByUser } from "./recipientCalls";
+
 const apiUrl = process.env.REACT_APP_API_URL;
 
 const fetchMessagesByUser = async (token) => {
@@ -41,4 +43,45 @@ const sendNewMessage = async (token, messageValues) => {
   }
 };
 
-export { fetchMessagesByUser, sendNewMessage };
+
+const messagesPerRecipient = async (authToken) => {
+  let messages = await fetchMessagesByUser(authToken)
+  let recipients = await fetchRecipientsByUser(authToken)
+
+  const getRecipientName = (recipientArray) => {
+    let userArray = []
+    for (var u = 0; u <= recipientArray.length - 1; u++) {
+      userArray.push([recipientArray[u].id, recipientArray[u].first_name])
+    }
+    return userArray
+  }
+  
+  const getRecipientMsgCount = (messages) => {
+    let userMsgCount = []
+    for (var u = 0; u <= messages.length - 1; u++) {
+      if (messages[u].recipient in userMsgCount) {
+        userMsgCount[messages[u].recipient] += 1 
+      } else {
+        userMsgCount[messages[u].recipient] = 1
+      }
+    }
+    return userMsgCount;
+  }
+
+  const getCountByName = (messages, recipients) => {
+    const idMsgCount = getRecipientMsgCount(messages)
+    const messagedRecipients = getRecipientName(recipients)
+    const dataArray = []
+
+    for (var u = 0; u <= messagedRecipients.length - 1; u++) {
+      dataArray.push({recipient: messagedRecipients[u][1], count: idMsgCount[messagedRecipients[u][0]] })
+    }
+    return dataArray
+  }
+
+  return (
+    getCountByName(messages, recipients)
+  )
+}
+
+export { fetchMessagesByUser, sendNewMessage, messagesPerRecipient};
--- src/components/RecipientChart/RecipientChart.js
+++ src/components/RecipientChart/RecipientChart.js
@@ -4,120 +4,130 @@ import { fetchRecipientsByUser } from "../../api/recipientCalls";
 import { fetchMessagesByUser } from "../../api/messageCalls";
 import { UserMessages } from "../../components/Messages/AllMessagesList.js"
 import { RadarChart, PolarGrid, PolarAngleAxis, PolarRadiusAxis, Radar, Legend, ResponsiveContainer, Tooltip, } from "recharts";
+import { messagesPerRecipient } from '../../api/messageCalls';
 
 const RecipientChart = () => {
   const [messageArray, setMessageArray] = useState([]);
   const [recipients, setRecipients] = useState([]);
   const [dataSet, setDataSet] = useState([]);
-
-  const { authToken } = useAuth();
-
-  useEffect( () => {
-    const getUserMessages = async () => {
-      let userMessages = await fetchMessagesByUser(authToken)
-      userMessages.map((message) => {
-        setMessageArray(...userMessages)
-      })
-      setMessageArray(userMessages)
-    }
-    if (authToken) {
-      if (messageArray.length == 0) {
-        getUserMessages()
-      }
-    }
-  }, []);
+  const { authToken, refreshUserData } = useAuth();
 
   useEffect( () => {
-    const getUserRecipients = async () => {
-      let userRecipients = await fetchRecipientsByUser(authToken)
-      setRecipients(userRecipients)
+    const getDataset = async() => {
+      setDataSet(await messagesPerRecipient(authToken))
     }
-    if (authToken) {
-      if (recipients.length == 0) {
-        getUserRecipients()
-      }
-    }
-  }, []);
+    getDataset()
+  }, [authToken, refreshUserData])
 
-  useEffect( () => {
-    const getData = async () => {
-      const finalData = await buildDataArray()
-      setDataSet(finalData)
-    }
-    if (authToken) {
-      if (dataSet.length == 0) {
-        getData()
-      }
-    }
-  }, [dataSet]);
+  // useEffect( () => {
+  //   console.log("RUN useEffect #1", refreshUserData)
+  //   const getUserMessages = async () => {
+  //     let userMessages = await fetchMessagesByUser(authToken)
+  //     userMessages.map((message) => {
+  //       setMessageArray(...userMessages)
+  //     })
+  //     setMessageArray(userMessages)
+  //   }
+  //   if (authToken) {
+  //     // if (messageArray.length == 0) {
+  //       getUserMessages()
+  //     // }
+  //   }
+  // }, [refreshUserData]);
+
+  // useEffect( () => {
+  //   const getUserRecipients = async () => {
+  //     let userRecipients = await fetchRecipientsByUser(authToken)
+  //     setRecipients(userRecipients)
+  //   }
+  //   if (authToken) {
+  //     if (recipients.length == 0) {
+  //       getUserRecipients()
+  //     }
+  //   }
+  // }, [authToken]);
+
+  // useEffect( () => {
+  //   // const getData = async () => {
+  //   //   const finalData = await buildDataArray()
+  //   const getData =  () => {
+  //     const finalData =  buildDataArray()
+  //     setDataSet(finalData)
+  //   }
+  //   if (authToken) {
+  //     if (dataSet.length == 0) {
+  //       getData()
+  //     }
+  //   }
+  // }, [dataSet]);
 
 
-  const buildDataArray = () => {
-    const dataArray = []
+  // const buildDataArray = () => {
+  //   const dataArray = []
 
-    const getRecipientName = (recipientArray) => {
-      let userArray = []
-      for (var u = 0; u <= recipientArray.length - 1; u++) {
-        userArray.push([recipientArray[u].id, recipientArray[u].first_name])
-      }
-      return userArray
-    }
+  //   const getRecipientName = (recipientArray) => {
+  //     let userArray = []
+  //     for (var u = 0; u <= recipientArray.length - 1; u++) {
+  //       userArray.push([recipientArray[u].id, recipientArray[u].first_name])
+  //     }
+  //     return userArray
+  //   }
   
-    const getRecipientMsgCount = (messages) => {
-      let userMsgCount = []
-      for (var u = 0; u <= messages.length - 1; u++) {
-        if (messages[u].recipient in userMsgCount) {
-          userMsgCount[messages[u].recipient] += 1 
-        } else {
-          userMsgCount[messages[u].recipient] = 1
-        }
-      }
-      return userMsgCount;
-    }
+  //   const getRecipientMsgCount = (messages) => {
+  //     let userMsgCount = []
+  //     for (var u = 0; u <= messages.length - 1; u++) {
+  //       if (messages[u].recipient in userMsgCount) {
+  //         userMsgCount[messages[u].recipient] += 1 
+  //       } else {
+  //         userMsgCount[messages[u].recipient] = 1
+  //       }
+  //     }
+  //     return userMsgCount;
+  //   }
 
-    const getCountByName = () => {
-      const idMsgCount = getRecipientMsgCount(messageArray)
-      const messagedRecipients = getRecipientName(recipients)
+  //   const getCountByName = () => {
+  //     const idMsgCount = getRecipientMsgCount(messageArray)
+  //     const messagedRecipients = getRecipientName(recipients)
 
-      for (var u = 0; u <= messagedRecipients.length - 1; u++) {
-        dataArray.push({recipient: messagedRecipients[u][1], count: idMsgCount[messagedRecipients[u][0]] })
-      }
-      return dataArray
-    }
-    const userDataArray = getCountByName()
-    return userDataArray
-  }
+  //     for (var u = 0; u <= messagedRecipients.length - 1; u++) {
+  //       dataArray.push({recipient: messagedRecipients[u][1], count: idMsgCount[messagedRecipients[u][0]] })
+  //     }
+  //     return dataArray
+  //   }
+  //   const userDataArray = getCountByName()
+  //   return userDataArray
+  // }
 
-  const dataTest = [
-    {
-      recipient: "Bob",
-      count: 12,
-    },
-    {
-      recipient: "Dave",
-      count: 9,
-    },
-    {
-      recipient: "Alice",
-      count: 6,
-    },
-    {
-      recipient: "Doug",
-      count: 3,
-    },
-    {
-      recipient: "Liz",
-      count: 5,
-    },
-    {
-      recipient: "Sally",
-      count: 1,
-    },
-    {
-      recipient: "Matt",
-      count: 15,
-    },
-  ];
+  // const dataTest = [
+  //   {
+  //     recipient: "Bob",
+  //     count: 12,
+  //   },
+  //   {
+  //     recipient: "Dave",
+  //     count: 9,
+  //   },
+  //   {
+  //     recipient: "Alice",
+  //     count: 6,
+  //   },
+  //   {
+  //     recipient: "Doug",
+  //     count: 3,
+  //   },
+  //   {
+  //     recipient: "Liz",
+  //     count: 5,
+  //   },
+  //   {
+  //     recipient: "Sally",
+  //     count: 1,
+  //   },
+  //   {
+  //     recipient: "Matt",
+  //     count: 15,
+  //   },
+  // ];
 
   return (
     <ResponsiveContainer height={500} width="100%">
